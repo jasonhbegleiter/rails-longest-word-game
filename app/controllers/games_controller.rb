5a31ca5f9a3a387e5f1c2d3573158ae5f7e441c9
Jason Begleiter <jhbegs204@gmail.com>
2018-05-27 15:19:57 +0300

    small UI tweak

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,22 +12,29 @@ class GamesController < ApplicationController
   end
 
   def score
-    @exists = true
-    @letters = params[:letters].split("")
+    exists = true
+    letters = params[:letters].split("")
     guess_letters = params[:word].split("")
     guess_letters.each do |guess_letter|
-      if @letters.index(guess_letter)
-        @letters.delete(guess_letter)
+      if letters.index(guess_letter)
+        letters.delete(guess_letter)
       else
-        @exists = false
+        exists = false
         # break
       end
     end
-    if @exists && params[:word].length > 0
+    if exists && params[:word].length > 0
       url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
       serialized = open(url).read
-      @response = JSON.parse(serialized)
+      response = JSON.parse(serialized)
       # raise
+    end
+    if !exists
+      @output_text = "Dude, your word doesn't even use the right letters"
+    elsif !response["found"]
+      @output_text = "This isn't a real word, try again"
+    else
+      @output_text = "Nice work! Your score is #{response["length"]}"
     end
   end
 end
